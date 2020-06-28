56b11927050ed6e1cae56211b01b8a6822d5a770
luc <luchao@rimionline.com>
2020-06-28 17:05:57 +0800

    mybatis association

--- day03/src/main/java/com/rimi/day03/mapper/HuJiDiMapper.java
+++ day03/src/main/java/com/rimi/day03/mapper/HuJiDiMapper.java
@@ -1,10 +1,12 @@
 package com.rimi.day03.mapper;
 
+import com.rimi.day03.bean.HuJiDi;
+
 /**
  * @Author luc
  * @Date 2020/6/28
  */
 public interface HuJiDiMapper {
 
-    HuJiDiMapper findHuJiDiById(Integer id);
+    HuJiDi findHuJiDiById(Integer id);
 }
